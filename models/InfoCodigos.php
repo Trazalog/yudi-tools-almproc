937b2bcd713714cb97c21253129a4693e18c047f
Hugo Gallardo <hugoeduardogallardo@gmail.com>
2021-08-03 14:39:08 -0300

    agregado de modales en vistas de impresion

---
+++ models/InfoCodigos.php
@@ -0,0 +1,16 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/**
+* Entidad InfoCodigo encargada de obtener informacion
+* para representar codigos QR o de Barras
+*
+* @autor Hugo Gallardo
+*/
+class InfoCodigos extends CI_Model
+{
+		function __construct()
+		{
+			parent::__construct();
+		}
+
+
+}
