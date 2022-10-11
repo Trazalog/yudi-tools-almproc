1ebe64da633d3ee72d6e97d87b03ce691cb2f911
Rogelio Sanchez <rnsanchez@trazalog.com>
2022-10-11 14:51:36 -0300

    se agrego una forma de validar al tarea tomada

--- views/tareas/pedido_reparacion_neumaticos/view_desmontaje.php
+++ views/tareas/pedido_reparacion_neumaticos/view_desmontaje.php
@@ -25,36 +25,34 @@ initForm();
 
 async function cerrarTarea() {
     wo();
-    var frm_info_id =  await frmGuardarConPromesa($('#form-dinamico').find('form'));
-
-    if ($('#rechazo').prop('checked') && $('#motivo .form-control').val() == '') {
+    if (task.idUsuarioAsignado == '') {
         wc();
-        error('Error','Completar Motivo de Rechazo');
+        notificar('Alerta','<b>Primero debe tomar la tarea</b>','warning');
         return;
     }
+    var frm_info_id =  await frmGuardarConPromesa($('#form-dinamico').find('form'));
+    var id = $('#taskId').val();
+    var dataForm = new FormData($('#generic_form')[0]);
+    dataForm.append('taskId', $('#taskId').val());
+    dataForm.append('frm_info_id', frm_info_id);
 
-   var id = $('#taskId').val();
-   var dataForm = new FormData($('#generic_form')[0]);
-   dataForm.append('taskId', $('#taskId').val());
-   dataForm.append('frm_info_id', frm_info_id);
-
-   $.ajax({
-       type: 'POST',
-       data: dataForm,
-       cache: false,
-       contentType: false,
-       processData: false,
-       url: '<?php base_url() ?>index.php/<?php echo BPM ?>Proceso/cerrarTarea/' + id,
-       success: function(data) {
-			var fun = () => {linkTo('<?php echo BPM ?>Proceso/');}
-			confRefresh(fun);
-		},
-        error: function(data) {
-			error("Error",'Se produjo un error al cerrar la tarea');
-		},
-		complete: () => {
-			wc();
-		}
-   });
+    $.ajax({
+        type: 'POST',
+        data: dataForm,
+        cache: false,
+        contentType: false,
+        processData: false,
+        url: '<?php base_url() ?>index.php/<?php echo BPM ?>Proceso/cerrarTarea/' + id,
+        success: function(data) {
+                var fun = () => {linkTo('<?php echo BPM ?>Proceso/');}
+                confRefresh(fun);
+            },
+            error: function(data) {
+                error("Error",'Se produjo un error al cerrar la tarea');
+            },
+            complete: () => {
+                wc();
+            }
+    });
 }
 </script>
