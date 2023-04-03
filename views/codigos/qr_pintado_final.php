ae4161108ee54207ad04fa4f7d33b39466759c82
Pablo Marrelli <pmarrelli@trazalog.com>
2023-04-03 09:30:13 -0300

    actualizacion qr yudica

--- views/codigos/qr_pintado_final.php
+++ views/codigos/qr_pintado_final.php
@@ -1,37 +1,55 @@
 <style>
 
 #numero_orden_final{
-  margin-top:-1%;
-  font-size:95px;
+  margin-top:-15%; 
+  font-size:90px;
+  float: left;
+  height:100px;
+  margin-left:-5%;
+  width: 300px;
 }
-#codigoImage{
-  padding-top:5%;
-  padding-bottom: 0%;
-  padding-left:45%;
+
+#contenedorCodigo{
+  float:right;
+  margin-right: -1px;
+  height:120px;
+  width:130px;
+} 
+#imagenYudiFinal{
+  width: 250px;
+  height: 75px;
+  float:left;
+  margin-top:-70%;
+  margin-left:-10%;
 }
-#imagenYudi{
-  width: 150px;
-  height: 80px;
-  padding-left:0%;
-}   
 #tabla  {
   border: 1px solid #000 ;
   font-size: 25px;
 }
+#cliente_etiquetaFin{
+    font-size: 30px;
+}
+.box{
+  border-top:none;
+}
 </style>
 <div class='row'>
-  <div id="numOrdenEtiquetaFinal" class='col-md-6 col-sm-6 col-xs-6'>
+  <div id="numOrdenEtiquetaFinal" class='col-md-6 col-sm-6 col-xs-6 box'>
     <p id='numero_orden_final'><?php echo " ".$N_orden; ?></p>
   </div>
   <div class='col-md-6 col-sm-6 col-xs-6' id='contenedorImagenYudi'>
-    <img src="<?php echo base_url() ?>imagenes/yudica/logoYudica.png" alt="YUDICA NEUMATICOS" width="180" height="50" id='imagenYudi'>
+    <img src="<?php echo base_url() ?>imagenes/yudica/logoYudica.png" width="150" height="80" alt="YUDICA NEUMATICOS" id='imagenYudiFinal'>
   </div>
   <div id="contenedorTablaEtiquetaFinal" class='col-md-12 col-sm-12'>	
     <table class="table table-hover table-bordered table-sm" id="tabla">
       <tbody>
+        <tr>	
+          <td class='thead-dark'><strong>Fecha Fin</strong></td>
+          <td class='thead-dark'><strong><?php echo $Fecha_entrega ?></strong></td>
+        </tr>
         <tr>
           <td><strong>Cliente</strong></td>
-          <td class='thead-dark'><strong><p><?php echo $Cliente ?></p></strong></td>
+          <td class='thead-dark' id="cliente_etiquetaFin"><strong><p><?php echo $Cliente ?></p></strong></td>
         </tr>
         <tr>	
           <td class='thead-dark'><strong>Zona</strong></td>
@@ -60,6 +78,10 @@
         <tr>
           <td class='thead-dark'><strong>Cantidad</strong></td>
           <td class='thead-dark'><strong><?php echo $Num ?></strong></td>
+        </tr>
+        <tr>
+          <td class='thead-dark'><strong>Pedido Tango</strong></td>
+          <td class='thead-dark'><strong><?php echo $int_pedi_id ?></strong></td>
         </tr>
       </tbody>
     </table>
--- views/codigos/qr_pintado_final_footer.php
+++ views/codigos/qr_pintado_final_footer.php
@@ -1,6 +1,16 @@
+<style>
+  #pagina_web{
+    text-align: center;
+    font-size: 30px;
+  }
+  #div_footer{
+    text-align: center;
+  }
+</style>
+
 <div class="row">
   <div class="col-md-12 center-block" id='div_footer'>
-    <img  src="<?php echo base_url() ?>imagenes/yudica/LogoISO-VIPAL.png" alt="YUDICA NEUMATICOS">
+    <img id="imagen_footer" src="<?php echo base_url() ?>imagenes/yudica/LogoISO-VIPAL2.0.png" alt="YUDICA NEUMATICOS">
+    <p id="pagina_web"><strong>www.yudicaneumaticos.com</strong></p>
   </div>
- 
 </div>
